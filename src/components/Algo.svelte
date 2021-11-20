a02589d1d899e4166052d0a7a90ba2e4d4853e69
Edgar Hernandez <edgar.hernandezvh@gmail.com>
2021-11-19 23:53:21 -0600

    first commit

---
+++ src/components/Algo.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let algoD
+</script>
+
+<main>
+  <p >Algo desde algo</p>
+  <ul>
+	{#each algoD as item}
+		<li>{item.title}</li>
+	{/each}
+</ul>
+</main>
+
+<style>
+  p{
+    font-size:30px;
+    font-weight:600
+  }
+  ul{
+    list-style: none;
+  }
+</style>
